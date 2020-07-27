99073c58c8365d50934972a0c400f72da7efcd3b
Justice <justice.addico.ja@gmail.com>
2020-07-27 10:26:50 +0000

    Fixed tooltip bug

--- src/lib/smart-tooltip-angular.directive.ts
+++ src/lib/smart-tooltip-angular.directive.ts
@@ -21,8 +21,8 @@ export class SmartTooltipAngularDirective implements OnInit {
   @HostListener("mouseleave") mouseLeave() {
     this.mouseLeft(this.elementRef);
   }
-
-  constructor(private elementRef: ElementRef) {
+  
+  constructor(private elementRef:ElementRef) {
 
     this.elementRef.nativeElement.classList.add("smart-tooltip-container");
 
@@ -30,136 +30,148 @@ export class SmartTooltipAngularDirective implements OnInit {
     var cs = document.createElement('style');
     cs.type = 'text/css';
     cs.innerHTML = `
-              .smart-tooltip-container {
-                position: relative;
-          }
+            .smart-tooltip-container {
+              position: relative;
+        }
 
-          .smart-tooltip-container:hover .smart-tooltip {
-                visibility: visible;
-                opacity: 1;
-          }
+        .smart-tooltip-container:hover .smart-tooltip {
+              visibility: visible;
+              opacity: 1;
+        }
 
-          .smart-tooltip {
-                position: absolute;
-                background-color: #333;
-                border-radius: 5px;
-                color: #eee;
-                padding: 10px 12px;
-                z-index: 100000;
-                top: 50%;
-                -webkit-transform: translate(-50%, -50%);
-                transform: translate(-50%, -50%);
-                left: 50%;
-                visibility: collapse;
-                transition: all .2s;
-                opacity: 0;
-                outline: none;
-          }
 
-          .smart-tooltip.left-t {
-                -webkit-transform: none;
-                transform: none;
-                right: calc(100% + 5px);
-                left: initial;
-                top: initial;
-                bottom: 5px;
 
-                
-          }
+        .smart-tooltip {
+              position: absolute;
+              background-color: #333;
+              border-radius: 5px;
+              color: #eee;
+              padding: 10px 12px;
+              z-index: 100000;
+              top: 50%;
+              -webkit-transform: translate(-50%, -50%);
+              transform: translate(-50%, -50%);
+              left: 50%;
+              visibility: collapse;
+              transition: all .2s;
+              opacity: 0;
+              outline: none;
+        }
 
-          .smart-tooltip.left-b {
-                -webkit-transform: none;
-                transform: none;
-                right: calc(100% + 5px);
-                left: initial;
-                top: 5px;
-                bottom: initial;
-          }
+        .smart-tooltip.left-t {
+              -webkit-transform: none;
+              transform: none;
+              right: calc(100% + 5px);
+              left: initial;
+              top: initial;
+              bottom: 5px;
 
-          .smart-tooltip.left {
-                right: calc(100% + 5px);
-                left: initial;
-                top: 50%;
-                -webkit-transform: translateY(-50%);
-                transform: translateY(-50%);
-          }
+              
+        }
 
-          .smart-tooltip.right-b {
-                -webkit-transform: none;
-                transform: none;
-                left: calc(100% + 5px);
-                top: 5px;
-                right: initial;
-                bottom: initial;    
-          }
+        .smart-tooltip.left-b {
+              -webkit-transform: none;
+              transform: none;
+              right: calc(100% + 5px);
+              left: initial;
+              top: 5px;
+              bottom: initial;
+        }
 
-          .smart-tooltip.right-t {
-                -webkit-transform: none;
-                transform: none;
-                /* display: none; */
-                left: calc(100% + 5px);
-                top: initial;
-                bottom: 5px;
-          }
+        .smart-tooltip.left {
+              right: calc(100% + 5px);
+              left: initial;
+              top: 50%;
+              -webkit-transform: translateY(-50%);
+              transform: translateY(-50%);
+        }
 
-          .smart-tooltip.right {
-                left: calc(100% + 5px);
-                top: 50%;
-                bottom: initial;
-                right: initial;
-                -webkit-transform: translateY(-50%);
-                transform: translateY(-50%);
-          }
+        .smart-tooltip.right-b {
+              -webkit-transform: none;
+              transform: none;
+              left: calc(100% + 5px);
+              top: 5px;
+              right: initial;
+              bottom: initial;    
+        }
 
-          .smart-tooltip.bottom-r {
-                /* display: none; */
-                top: calc(100% + 5px);
-                bottom: initial;
-                left: 5px;
-                right:initial;
-                transform: none;
-          }
-          .smart-tooltip.bottom-l {
-                /* display: none; */
-                top: calc(100% + 5px);
-                bottom: initial;
-                right: 5px;
-                left:initial;
-                transform: none;
-          }
-          .smart-tooltip.bottom {
-                /* display: none; */
-                top: calc(100% + 5px);
-                bottom: initial;
-                left: 50%;
-                transform: translateX(-50%);
-          }
+        .smart-tooltip.right-t {
+              -webkit-transform: none;
+              transform: none;
+              /* display: none; */
+              left: calc(100% + 5px);
+              top: initial;
+              bottom: 5px;
+        }
 
-          .smart-tooltip.top-r {
-                /* display: none; */
-                bottom: calc(100% + 5px);
-                top: initial;
-                left: 5px;
-                right:initial;
-                transform: none;
-          }
-          .smart-tooltip.top-l {
-                /* display: none; */
-                bottom: calc(100% + 5px);
-                top: initial;
-                right: 5px;
-                left: initial;
-                transform: none;
-          }
-          .smart-tooltip.top {
-                /* display: none; */
-                bottom: calc(100% + 5px);
-                top: initial;
-                left: 50%;
-                transform: translateX(-50%);
-          }
+        .smart-tooltip.right {
+              left: calc(100% + 5px);
+              top: 50%;
+              bottom: initial;
+              right: initial;
+              -webkit-transform: translateY(-50%);
+              transform: translateY(-50%);
+        }
+
+        .smart-tooltip.bottom-r {
+              /* display: none; */
+              top: calc(100% + 5px);
+              bottom: initial;
+              left: 5px;
+              right:initial;
+              transform: none;
+        }
+        .smart-tooltip.bottom-l {
+              /* display: none; */
+              top: calc(100% + 5px);
+              bottom: initial;
+              right: 5px;
+              left:initial;
+              transform: none;
+        }
+        .smart-tooltip.bottom {
+              /* display: none; */
+              top: calc(100% + 5px);
+              bottom: initial;
+              left: 50%;
+              transform: translateX(-50%);
+        }
+
+        .smart-tooltip.top-r {
+              /* display: none; */
+              bottom: calc(100% + 5px);
+              top: initial;
+              left: 5px;
+              right:initial;
+              transform: none;
+        }
+        .smart-tooltip.top-l {
+              /* display: none; */
+              bottom: calc(100% + 5px);
+              top: initial;
+              right: 5px;
+              left: initial;
+              transform: none;
+        }
+        .smart-tooltip.top {
+              /* display: none; */
+              bottom: calc(100% + 5px);
+              top: initial;
+              left: 50%;
+              transform: translateX(-50%);
+        }
+
+        .smart-tooltip-container .smart-tooltip.overlayed{
+            
+              transform: none;
+              top:initial;
+              right:initial;
+              bottom:initial;
+              left:initial;
+        }
     `;
     head.appendChild(cs);
+
   }
 
   ngOnInit(): void {
@@ -172,7 +184,7 @@ export class SmartTooltipAngularDirective implements OnInit {
 
     let btn = elementRef.nativeElement;
     if (!btn) return;
-    var body = document.querySelector("body");
+    //var body = document.querySelector("body");
 
     let tooltip = btn.querySelector(".smart-tooltip");
 
@@ -184,15 +196,16 @@ export class SmartTooltipAngularDirective implements OnInit {
 
     var tooltipclient = tooltip.getBoundingClientRect();
     var btnclient = btn.getBoundingClientRect();
-    var bodyclient = body.getClientRects()[0];
+    //var bodyclient = body.getClientRects()[0];
 
-    console.log(bodyclient);
+    let windowWidth = window.innerWidth;
+    let windowHeight = window.innerHeight;
 
     var spaces = {
       left: btnclient.left > tooltipclient.width,
-      right: (bodyclient.width - btnclient.right) > tooltipclient.width,
+      right: (windowWidth - btnclient.right) > tooltipclient.width,
       top: btnclient.top > tooltipclient.height,
-      bottom: (bodyclient.height - btnclient.bottom) > tooltipclient.height,
+      bottom: (windowHeight - btnclient.bottom) > tooltipclient.height,
       getSpace: function (key) {
         let stayClass = root.getStayClass(tooltip);
         // debugger;
@@ -211,7 +224,7 @@ export class SmartTooltipAngularDirective implements OnInit {
     if (spaces.getSpace("left")) {
 
       let topHalf = (btnclient.bottom - (btnclient.height / 2));
-      let bottomHalf = bodyclient.height - topHalf;
+      let bottomHalf = windowHeight - topHalf;
       let halfTooltip = (tooltipclient.height / 2);
 
       let isContainerBigger = btnclient.height >= tooltipclient.height;
@@ -235,7 +248,7 @@ export class SmartTooltipAngularDirective implements OnInit {
     } else if (spaces.getSpace("right")) {
 
       let topHalf = (btnclient.bottom - (btnclient.height / 2));
-      let bottomHalf = bodyclient.height - topHalf;
+      let bottomHalf = windowHeight - topHalf;
       let halfTooltip = (tooltipclient.height / 2);
 
       let isContainerBigger = btnclient.height >= tooltipclient.height;
@@ -257,7 +270,7 @@ export class SmartTooltipAngularDirective implements OnInit {
     } else if (spaces.getSpace("top")) {
 
       let leftHalf = (btnclient.right - (btnclient.width / 2));
-      let rightHalf = bodyclient.width - leftHalf;
+      let rightHalf = windowWidth - leftHalf;
       let halfTooltip = (tooltipclient.width / 2);
 
       let isContainerBigger = btnclient.width >= tooltipclient.width;
@@ -280,7 +293,7 @@ export class SmartTooltipAngularDirective implements OnInit {
     } else if (spaces.getSpace("bottom")) {
       // debugger;
       let leftHalf = (btnclient.right - (btnclient.width / 2));
-      let rightHalf = bodyclient.width - leftHalf;
+      let rightHalf = windowWidth - leftHalf;
       let halfTooltip = (tooltipclient.width / 2);
 
       let isContainerBigger = btnclient.width >= tooltipclient.width;
@@ -335,8 +348,8 @@ export class SmartTooltipAngularDirective implements OnInit {
   }
 
 
-  mouseLeft(elementRef:ElementRef) {
-    
+  mouseLeft(elementRef: ElementRef) {
+
     let btn = elementRef.nativeElement;
     if (!btn) return;
 
@@ -395,19 +408,22 @@ export class SmartTooltipAngularDirective implements OnInit {
    * @param {string} tooltipPostion The position the tooltip should be displayed
    */
   getOverlayCoordinates(btnclient, tooltipclient, tooltipPostion) {
-    
-    let bodyclient = document.querySelector("body").getClientRects()[0];
+    // let body:any = document.querySelector("body");
+    // let bodyclient = body.getClientRects()[0];
+
+    let windowWidth = window.innerWidth;
+    let windowHeight = window.innerHeight;
 
     if (tooltipPostion.includes("right")) {
 
-      let overlayCoordinates:any = {
+      let overlayCoordinates: any = {
         left: btnclient.right + this.offSetValue,
         top: (btnclient.top + (btnclient.height / 2)) - (tooltipclient.height / 2),
         props: ["left", "top"]
       }
 
       if (tooltipPostion == "right-t") {
-        overlayCoordinates.bottom = bodyclient.height - btnclient.bottom;
+        overlayCoordinates.bottom = windowHeight - btnclient.bottom;
         overlayCoordinates.props = ["left", "bottom"];
       } else if (tooltipPostion == "right-b") {
         overlayCoordinates.top = btnclient.top + this.offSetValue;
@@ -415,15 +431,15 @@ export class SmartTooltipAngularDirective implements OnInit {
 
       return overlayCoordinates;
     } else if (tooltipPostion.includes("left")) {
-
-      let overlayCoordinates:any = {
-        right: (bodyclient.width - btnclient.left) + this.offSetValue,
+     
+      let overlayCoordinates: any = {
+        right: (windowWidth - btnclient.left) + this.offSetValue,
         top: (btnclient.top + (btnclient.height / 2)) - (tooltipclient.height / 2),
         props: ["right", "top"]
       }
 
       if (tooltipPostion == "left-t") {
-        overlayCoordinates.bottom = bodyclient.height - btnclient.bottom;
+        overlayCoordinates.bottom = windowHeight - btnclient.bottom;
         overlayCoordinates.props = ["right", "bottom"];
       } else if (tooltipPostion == "left-b") {
         overlayCoordinates.top = btnclient.top + this.offSetValue;
@@ -432,14 +448,14 @@ export class SmartTooltipAngularDirective implements OnInit {
       return overlayCoordinates;
     } else if (tooltipPostion.includes("top")) {
 
-      let overlayCoordinates:any = {
+      let overlayCoordinates: any = {
         left: (btnclient.right - (btnclient.width / 2)) - (tooltipclient.width / 2),
-        bottom: (bodyclient.height - btnclient.top) + this.offSetValue,
+        bottom: (windowHeight - btnclient.top) + this.offSetValue,
         props: ["left", "bottom"]
       }
 
       if (tooltipPostion == "top-l") {
-        overlayCoordinates.right = (bodyclient.width - btnclient.right) + this.offSetValue;
+        overlayCoordinates.right = (windowWidth - btnclient.right) + this.offSetValue;
         overlayCoordinates.props = ["right", "bottom"]
       } else if (tooltipPostion == "top-r") {
         overlayCoordinates.left = btnclient.left + this.offSetValue;
@@ -448,14 +464,14 @@ export class SmartTooltipAngularDirective implements OnInit {
       return overlayCoordinates;
     } else if (tooltipPostion.includes("bottom")) {
 
-      let overlayCoordinates:any = {
+      let overlayCoordinates: any = {
         left: (btnclient.right - (btnclient.width / 2)) - (tooltipclient.width / 2),
         top: btnclient.bottom + this.offSetValue,
         props: ["left", "top"]
       }
 
       if (tooltipPostion == "bottom-l") {
-        overlayCoordinates.right = (bodyclient.width - btnclient.right) + this.offSetValue;
+        overlayCoordinates.right = (windowWidth - btnclient.right) + this.offSetValue;
         overlayCoordinates.props = ["right", "top"]
       } else if (tooltipPostion == "bottom-r") {
         overlayCoordinates.left = btnclient.left + this.offSetValue;
